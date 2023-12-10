e26d581af6d9c6eae61123a01d643bdbae1c2647
jwhgzs <admin@jwhgzs.com>
2023-12-10 11:05:33 +0800

    upload

--- ginit.sh
+++ ginit.sh
@@ -2,7 +2,7 @@
 sdir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 token=`cat "$sdir/config/token.txt"`
 user=`cat "$sdir/config/user.txt"`
-repo="${PWD##*/}"
+read -p "* Please enter your repo name: " repo
 url="https://${token}@github.com/${user}/${repo}.git"
 git init
 git remote add origin "${url}"
